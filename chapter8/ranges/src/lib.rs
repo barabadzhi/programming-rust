2f8d6c39d4b168edd78d2cdd55e81638c66895d0
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-03-01 23:51:35 +0300

    Implement doc-tests example crate

---
+++ chapter8/ranges/src/lib.rs
@@ -0,0 +1,15 @@
+use std::ops::Range;
+
+/// Return true if two ranges overlap.
+///
+///     assert_eq!(ranges::overlap(0..7, 3..10), true);
+///     assert_eq!(ranges::overlap(1..5, 101..105), false);
+///
+/// If either range is empty, they don't count as overlapping.
+///
+///     assert_eq!(ranges::overlap(0..0, 0..10), false);
+///     assert_eq!(ranges::overlap(0..10, 1..1), false);
+///
+pub fn overlap(r1: Range<usize>, r2: Range<usize>) -> bool {
+    r1.start < r1.end && r2.start < r2.end && r1.start < r2.end && r2.start < r1.end
+}
